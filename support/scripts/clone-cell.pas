f9a156607078b110cd01eb64fe77a3d6c810a807
Dan Cassidy <5643061+rux616@users.noreply.github.com>
2023-11-22 18:49:11 +1300

    modularize the script also comments out most of the chatty bits

--- support/scripts/clone-cell.pas
+++ support/scripts/clone-cell.pas
@@ -1,6 +1,5 @@
 {
-  New script template, only shows processed records
-  Assigning any nonzero value to Result will terminate script
+  Do a full clone of any selected cells and their respective subrecords as new records
 }
 unit CloneCell;
 
@@ -9,10 +8,8 @@ unit CloneCell;
 // determine whether to deal with worldspaces
 // remove "temporary" group if empty when copying persistent records
 
+var to_file: IwbFile;
 
-const
-  group_type_cell_persistent_children = 8;
-  group_type_cell_temporary_children = 9;
 // var NewCell, OldCell : IwbContainer;
 //var ValidCopySignatures;
 // cell record signature
@@ -30,87 +27,153 @@ const
 // PHZD - Placed Hazard
 // NAVM - Navigation Mesh
 
-// Called before processing
-// You can remove it if script doesn't require initialization code
-function Initialize: integer;
+
+procedure CloneRecordElements(old_record, new_record: IInterface);
+var
+  i: integer;
+  element: IInterface;
 begin
-  Result := 0;
+  for i := 1 to Pred(ElementCount(old_record)) do begin
+    element := ElementByIndex(old_record, i);
+    wbCopyElementToRecord(element, new_record, False, True);
+    // AddMessage('i: ' + IntToStr(i) + ', Sig: ' + Signature(element) + ' Elements: ' + IntToStr(ElementCount(element)));
+  end;
 end;
 
 
-// called for every record selected in xEdit
-function Process(e: IInterface): integer;
+procedure CloneCellGroup(old_cell_group, new_cell: IInterface);
 var
-  i, j: integer;
-  old_cell_group, cell_element_record, cell_subrecord, subrecord_element_record, new_cell, new_subrecord: IInterface;
-  to_file: IwbFile;
+  i: integer;
+  old_cell_subrecord: IInterface;
+  new_cell_subrecord: IInterface;
 begin
-  Result := 0;
+  // create new subrecords
+  for i := 0 to Pred(ElementCount(old_cell_group)) do begin
+    old_cell_subrecord := ElementByIndex(old_cell_group, i);
+    // AddMessage('Subrecord: ' + ShortName(old_cell_subrecord) + ' ' + GetElementEditValues(old_cell_subrecord, 'NAME'));
+    // AddMessage('FullPath: ' + FullPath(old_cell_subrecord));
+    new_cell_subrecord := Add(new_cell, Signature(old_cell_subrecord), True);
 
-  if Signature(e) <> 'CELL' then
-    Exit;
+    // copy elements from old subrecord to new subrecord
+    CloneRecordElements(old_cell_subrecord, new_cell_subrecord);
 
-  to_file := GetFile(e);
-  AddMessage('Processing: ' + FullPath(e));
-  AddMessage('ShortName: ' + ShortName(e));
-  AddMessage('Elements: ' + IntToStr(ElementCount(e)));
-
-  // add new cell
-  new_cell := Add(GetContainer(GetContainer(GetContainer(e))), 'CELL', True);
-  AddMessage('New Cell: ' + IntToHex(FixedFormID(new_cell), 8));
-
-  // copy element records from old cell to new cell
-  for i := 1 to Pred(ElementCount(e)) do begin
-    cell_element_record := ElementByIndex(e, i);
-    wbCopyElementToRecord(cell_element_record, new_cell, False, True);
-    AddMessage('i: ' + IntToStr(i) + ', Sig: ' + Signature(cell_element_record) + ' Elements: ' + IntToStr(ElementCount(cell_element_record)));
+    SetIsPersistent(new_cell_subrecord, GetIsPersistent(old_cell_subrecord));
   end;
+end;
+
+
+procedure CloneCellGroups(old_cell, new_cell: IInterface);
+const
+  GROUP_TYPE_CELL_PERSISTENT_CHILDREN = 8;
+  GROUP_TYPE_CELL_TEMPORARY_CHILDREN = 9;
+var
+  old_cell_group: IInterface;
+begin
+  // create new subrecords for persistent subrecords
+  old_cell_group := FindChildGroup(ChildGroup(old_cell), GROUP_TYPE_CELL_PERSISTENT_CHILDREN, old_cell);
+  // AddMessage('# persistent children: ' + IntToStr(ElementCount(old_cell_group)));
+  CloneCellGroup(old_cell_group, new_cell);
 
   // create new subrecords for temporary subrecords
-  old_cell_group := FindChildGroup(ChildGroup(e), group_type_cell_temporary_children, e);
-  AddMessage('# temporary children: ' + IntToStr(ElementCount(old_cell_group)));
-  for i := 0 to Pred(ElementCount(old_cell_group)) do begin
-    cell_subrecord := ElementByIndex(old_cell_group, i);
-    AddMessage('Subrecord: ' + ShortName(cell_subrecord) + ' ' + GetElementEditValues(cell_subrecord, 'NAME'));
-    AddMessage('FullPath: ' + FullPath(cell_subrecord));
-    new_subrecord := Add(new_cell, Signature(cell_subrecord), True);
-
-    // copy element records from old subrecords to new subrecords
-    for j := 1 to Pred(ElementCount(cell_subrecord)) do begin
-      subrecord_element_record := ElementByIndex(cell_subrecord, j);
-      wbCopyElementToRecord(subrecord_element_record, new_subrecord, False, True);
-      AddMessage('j: ' + IntToStr(j) + ', Sig: ' + Signature(subrecord_element_record) + ' Elements: ' + IntToStr(ElementCount(subrecord_element_record)));
-    end;
-  end;
-
-  // create new subrecords for persistent subrecords
-  old_cell_group := FindChildGroup(ChildGroup(e), group_type_cell_persistent_children, e);
-  AddMessage('# persistent children: ' + IntToStr(ElementCount(old_cell_group)));
-  for i := 0 to Pred(ElementCount(old_cell_group)) do begin
-    cell_subrecord := ElementByIndex(old_cell_group, i);
-    AddMessage('Subrecord: ' + ShortName(cell_subrecord) + ' ' + GetElementEditValues(cell_subrecord, 'NAME'));
-    AddMessage('FullPath: ' + FullPath(cell_subrecord));
-    new_subrecord := Add(new_cell, Signature(cell_subrecord), True);
-
-    // copy element records from old subrecords to new subrecords
-    for j := 1 to Pred(ElementCount(cell_subrecord)) do begin
-      subrecord_element_record := ElementByIndex(cell_subrecord, j);
-      wbCopyElementToRecord(subrecord_element_record, new_subrecord, False, True);
-      AddMessage('j: ' + IntToStr(j) + ', Sig: ' + Signature(subrecord_element_record) + ' Elements: ' + IntToStr(ElementCount(subrecord_element_record)));
-    end;
-
-    SetIsPersistent(new_subrecord, True);
-  end;
-
+  old_cell_group := FindChildGroup(ChildGroup(old_cell), GROUP_TYPE_CELL_TEMPORARY_CHILDREN, old_cell);
+  // AddMessage('# temporary children: ' + IntToStr(ElementCount(old_cell_group)));
+  CloneCellGroup(old_cell_group, new_cell);
 end;
 
 
+function DoCloneCell(old_cell, to_file: IInterface): IInterface;
+var
+  group_cell, new_cell: IInterface;
+begin
+  if Signature(old_cell) <> 'CELL' then begin
+    Result := nil;
+    exit;
+  end;
 
-// Called after processing
-// You can remove it if script doesn't require finalization code
-function Finalize: integer;
+  if not HasGroup(to_file, 'CELL') then
+    Add(to_file, 'CELL', True);
+
+  group_cell := GroupBySignature(to_file, 'CELL');
+
+  new_cell := Add(group_cell, 'CELL', True);
+
+  // copy element records from old cell to new cell
+  CloneRecordElements(old_cell, new_cell);
+
+  // clone cell subrecords
+  CloneCellGroups(old_cell, new_cell);
+
+  Result := new_cell;
+end;
+
+
+function FileDialog(e: IInterface): IwbFile;
+var
+  i: integer;
+  frm: TForm;
+  clb: TCheckListBox;
+  to_file: IInterface;
 begin
   Result := 0;
+  frm := frmFileSelect;
+  try
+    frm.Caption := 'Select a plugin';
+    clb := TCheckListBox(frm.FindComponent('CheckListBox1'));
+    clb.Items.Add('<new file>');
+    for i := Pred(FileCount) downto 0 do begin
+      clb.Items.InsertObject(1, GetFileName(FileByIndex(i)), FileByIndex(i));
+      if GetFileName(e) = GetFileName(FileByIndex(i)) then begin
+        break;
+      end;
+    end;
+    if frm.ShowModal <> mrOk then begin
+      Result := nil;
+      Exit;
+    end;
+    for i := 0 to Pred(clb.Items.Count) do begin
+      if clb.Checked[i] then begin
+        if i = 0 then ToFile := AddNewFile else
+          to_file := ObjectToElement(clb.Items.Objects[i]);
+        Break;
+      end;
+    end;
+  finally
+    frm.Free;
+  end;
+  if not Assigned(to_file) then begin
+    Result := nil;
+    exit;
+  end;
+  AddRequiredElementMasters(e, to_file, False);
+  Result := to_file;
+end;
+
+
+function Process(e: IInterface): integer;
+var
+  i: integer;
+  new_cell: IInterface;
+begin
+  if Signature(e) <> 'CELL' then
+    exit;
+
+  if not Assigned(to_file) then begin
+    to_file := FileDialog(e);
+  end;
+  if not Assigned(to_file) then begin
+    Result := 1;
+    exit;
+  end;
+
+  AddMessage('Cloning ' + ShortName(e));
+  new_cell := DoCloneCell(e, to_file);
+
+  if Assigned(new_cell) then begin
+    AddMessage('Cloned to ' + ShortName(new_cell));
+    Result := 0;
+  else begin
+    Result := 1;
+  end;
 end;
 
 end.
